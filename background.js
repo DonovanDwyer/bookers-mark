645c80867b9179e33ba75b436401024abfe5795b
Donovan Dwyer <dwyer.donovan@gmail.com>
2022-07-07 20:02:22 -0400

    latest code

--- background.js
+++ background.js
@@ -1,5 +1,11 @@
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const targetFolder = async () => {
-    let x = await chrome.bookmarks.getTree();
+    let bookmarks = await chrome.bookmarks.getTree();
     function recursiveLookup(tree, acc){
         let arr = acc;
         for (const item of tree){
@@ -10,88 +16,46 @@ const targetFolder = async () => {
         }
         return arr;
     }
-    return recursiveLookup(x, []);
+    return recursiveLookup(bookmarks, []);
 }
 
-const openNextBookMark = async () => {
-    // console.log(await chrome.bookmarks.getTree());
-    let tree = await chrome.bookmarks.getSubTree("707");
-    tree = tree[0].children;
-
-    let bookmarks = [];
-    
-    const recursiveLookup = (elem, acc) => {
-        console.log(elem, acc);
-        let arr = acc;
-        for (let i = 0; i < elem.length; i++){
-            if (!elem[i].children) {
-                arr.push(elem[i]);
-            } else {
-                recursiveLookup(elem[i].children, arr);
+const openBookmark = async (random) => {
+    chrome.storage.sync.get(['targetFolder'], async (res) => {
+        let tree = await chrome.bookmarks.getSubTree(res.targetFolder.id);
+        tree = tree[0].children;
+        let bookmarks = [];
+        const recursiveLookup = (elem, acc) => {
+            const arr = acc;
+            for (let i = 0; i < elem.length; i++){
+                if (!elem[i].children) {
+                    arr.push(elem[i]);
+                } else {
+                    recursiveLookup(elem[i].children, arr);
+                }
             }
+            return arr;
         }
-        return arr;
-    }
-    bookmarks = recursiveLookup(tree, bookmarks);
-
-    let bookmark = bookmarks.shift();
-    await chrome.tabs.create({
-        active: true,
-        url: bookmark.url
+        bookmarks = recursiveLookup(tree, bookmarks);
+        const bookmark = random ? bookmarks[getRandomInt(0, bookmarks.length - 1)] : bookmarks.shift();
+        await chrome.tabs.create({
+            active: true,
+            url: bookmark.url
+        });
+        await chrome.bookmarks.remove(bookmark.id);
     });
-    await chrome.bookmarks.remove(bookmark.id);
 }
-
-const openRandoBookMark = async () => {
-    // console.log(await chrome.bookmarks.getTree());
-    let tree = await chrome.bookmarks.getSubTree("707");
-    tree = tree[0].children;
-
-    let bookmarks = [];
-    
-    const recursiveLookup = (elem, acc) => {
-        console.log(elem, acc);
-        let arr = acc;
-        for (let i = 0; i < elem.length; i++){
-            if (!elem[i].children) {
-                arr.push(elem[i]);
-            } else {
-                recursiveLookup(elem[i].children, arr);
-            }
-        }
-        return arr;
-    }
-    bookmarks = recursiveLookup(tree, bookmarks);
-
-    let length = bookmarks.length - 1;
-    let bookmark = bookmarks[getRandomInt(0, length)];
-    await chrome.tabs.create({
-        active: true,
-        url: bookmark.url
-    });
-    await chrome.bookmarks.remove(bookmark.id);
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message == 'get-bookmark') {
-        openNextBookMark();
+        openBookmark();
         sendResponse('Bookmark Get');
-    } else if (message == 'get-rando'){
-        openRandoBookMark();
+    } 
+    if (message == 'get-rando'){
+        openBookmark(true);
         sendResponse('Rando Get');
     }
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message == 'update-cache'){
         targetFolder().then(sendResponse);
         return true;
     }
-})
+});
--- popup.js
+++ popup.js
@@ -3,6 +3,22 @@ const select = document.getElementById('bookmark-folders');
 const getButton = document.getElementById('get-bookmark');
 const randoButton = document.getElementById('get-rando');
 
+(function getSelect(){
+    select.addEventListener('change', () => {
+        try {
+            chrome.storage.sync.set({ targetFolder: { 
+                id: select.value, 
+                title: select[select.selectedIndex].innerHTML,
+                index: select.selectedIndex
+            }
+        });
+        targetTitle.innerText = select[select.selectedIndex].innerHTML;
+        } catch (err){
+            console.error(err);
+        }
+    })
+})();
+
 (function getBookmark(){
     getButton.addEventListener('click', () => {
         try {
@@ -30,7 +46,25 @@ const randoButton = document.getElementById('get-rando');
 (async function updateCache(){
     try {
         await chrome.runtime.sendMessage('update-cache', (response) => {
-            console.log(response);
+            for (const folder of response){
+                let title, id;
+                if (folder.id === '0') {
+                    title = 'Root Folder';
+                    id = '0';
+                } else {
+                    title = folder.title === '' ? 'Untitled Folder' : folder.title;
+                    id = folder.id;
+                }
+                let option = document.createElement('option');
+                option.value = id;
+                option.append(document.createTextNode(title));
+                select.append(option);
+            }
+            chrome.storage.sync.get(['targetFolder'], (result) => {
+                let text = document.createTextNode(result.targetFolder.title);
+                targetTitle.append(text);
+                select.selectedIndex = result.targetFolder.index;
+            })
         })
     } catch (err) {
         console.error(err);
